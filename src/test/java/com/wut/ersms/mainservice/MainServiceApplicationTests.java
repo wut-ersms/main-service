e04edc7eb3f1261e47f7c631c7832d96e4bdea8f
dtomala <d.tomala@sages.com.pl>
2025-03-18 17:40:42 +0100

    Init repository

---
+++ src/test/java/com/wut/ersms/mainservice/MainServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wut.ersms.mainservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
